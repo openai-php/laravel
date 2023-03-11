c6bfc29673bf991fe8f1f89781b56a2a3f81aa0f
Sandro Gehri <sgehri@hosttech.ch>
2023-03-11 21:19:35 +0100

    commit missing file

---
+++ src/Testing/OpenAIFake.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OpenAI\Laravel\Testing;
+
+use Illuminate\Support\Testing\Fakes\Fake;
+use OpenAI\Testing\ClientFake;
+
+class OpenAIFake extends ClientFake implements Fake
+{
+}
