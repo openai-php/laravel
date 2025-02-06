fe3f03c8e3ef4a77352ca92b8e9f2e52b98f95ab
Evosite <60429439+evosite-ltd@users.noreply.github.com>
2025-02-06 14:59:40 +0000

    Simplify support for Project-based auth

--- config/openai.php
+++ config/openai.php
@@ -14,6 +14,7 @@ return [
 
     'api_key' => env('OPENAI_API_KEY'),
     'organization' => env('OPENAI_ORGANIZATION'),
+    'project' => env('OPENAI_PROJECT'),
 
     /*
     |--------------------------------------------------------------------------
