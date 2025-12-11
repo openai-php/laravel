e99ae520428608b687d752787f514ec1adb2bc53
Moritz Mazetti <moritz@matchory.com>
2025-12-11 16:23:27 +0100

    Remove copyright comments

--- src/Http/Middleware/VerifyWebhookSignature.php
+++ src/Http/Middleware/VerifyWebhookSignature.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * This file is part of openai-php-laravel, a Matchory application.
- *
- * Unauthorized copying of this file, via any medium, is strictly prohibited.
- * Its contents are strictly confidential and proprietary.
- *
- * @copyright 2020–2025 Matchory GmbH · All rights reserved
- * @author    Moritz Friedrich <moritz@matchory.com>
- */
-
-declare(strict_types=1);
-
 namespace OpenAI\Laravel\Http\Middleware;
 
 use Closure;
